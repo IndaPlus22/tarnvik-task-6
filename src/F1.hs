6650959ad552c053f9894457c75f8864ea3f3c0d
Tilde Tarnvik <tilde@Tildes-Dator.local>
2022-10-13 21:33:31 +0200

    everything works, finally

--- src/F1.hs
+++ src/F1.hs
@@ -3,15 +3,14 @@ module F1 where
 import Data.List  
 import Data.Char
 
+--Fibonacci 
 fib :: Integer -> Integer
-fib 0 = 0
-fib 1 = 1
-fib n 
-    | n <= 42 = fibList!!(fromIntegral n)
-    | otherwise = fib(n-1) + fib (n-2)
+fib n = fibList !! (fromIntegral n)
 
-fibList = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309,3524578,5702887,9227465,14930352,24157817,39088169,63245986,102334155,165580141,267914296]
+fibList = 0 : 1 : next fibList 
+    where next (x : y : xs) = (x+y) : next (y : xs)
 
+--Rövarspråk
 isConsonant x = not ((elem x)"aeyuio")
 
 rovarsprak :: String -> String
@@ -20,15 +19,13 @@ rovarsprak (x:xs)
     | isConsonant x = x : 'o' : x : rovarsprak xs
     | otherwise = x : rovarsprak xs
 
---I know its ugly but atleast it works 
 karpsravor :: String -> String
 karpsravor [] = []
-karpsravor (x:y:z:xs)
-    | isConsonant x = x : karpsravor xs
-    | otherwise = x : karpsravor (y:z:xs)
-karpsravor (x:y:xs) = x : karpsravor (y:xs)
-karpsravor (x:xs) = x : karpsravor xs 
+karpsravor (x:xs)
+    | isConsonant x = x : karpsravor (tail(tail xs))
+    | otherwise = x : karpsravor (xs)
 
+--Medellängd
 amountOfWords :: String -> Int
 amountOfWords [] = 0
 amountOfWords (x:[]) 
@@ -47,20 +44,18 @@ amountOfLetters (x:xs)
 medellangd :: String -> Double
 medellangd s = fromIntegral(amountOfLetters s) / fromIntegral(amountOfWords s)
 
-almostThere [] = []
-almostThere s = faktisktSkyfflande s : almostThere (removeSkyfflade s)
+--Skyffla
+--There has to be a better way but atleast this works 
+skyffla :: [a] -> [a]
+skyffla [] = []
+skyffla s = faktisktSkyfflande s ++ skyffla (removeSkyfflade s)
 
-almostAlmostThere s = concat (almostThere s)
-
---skyffla :: [a] -> [a]
-skyffla s = almostAlmostThere s
-
---removeSkyfflade :: [a] -> [a]
+removeSkyfflade :: [a] -> [a]
 removeSkyfflade [] = []
 removeSkyfflade (x:y:xs) = y : removeSkyfflade (xs)
 removeSkyfflade (x:xs) = removeSkyfflade (xs)
 
---faktisktSkyfflande :: [a] -> [a]
+faktisktSkyfflande :: [a] -> [a]
 faktisktSkyfflande [] = []
 faktisktSkyfflande (x:[]) = x : faktisktSkyfflande []
 faktisktSkyfflande (x:xs) = x : faktisktSkyfflande (tail xs)
